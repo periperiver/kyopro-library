c523acb31bef0f8bb35856966dd60877e403fd18
ShimoboTaiki <95344729+ShimoboTaiki@users.noreply.github.com>
2026-06-22 22:37:11 +0900

    fastio test

---
+++ verify/LC/fastio.test.cpp
@@ -0,0 +1,11 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/many_aplusb"
+#include "fastio.hpp"
+int main(){
+  int t;
+  rd(t);
+  while(t--){
+    long long a,b;
+    rd(a),rd(b);
+    wt(a+b),wt('\n');
+  }
+}
