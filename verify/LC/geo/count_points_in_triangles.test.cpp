6c98c070cc356058f4abcd7893e2780b01698efe
ShimoboTaiki <95344729+ShimoboTaiki@users.noreply.github.com>
2026-06-24 23:39:33 +0900

    a

--- verify/LC/geo/count_points_in_triangles.test.cpp
+++ verify/LC/geo/count_points_in_triangles.test.cpp
@@ -1,11 +1,14 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/count_points_in_triangle"
 #include "geo/count_points_in_triangles.hpp"
 int main(){
-  int n,m;
-  std::cin>>n>>m;
-  std::vector<Point<long long>>a(n),b(m);
-  for(int i=0;i<n;i++)std::cin>>a[i];
-  for(int i=0;i<m;i++)std::cin>>b[i];
+  int n;
+  std::cin>>n;
+  std::vector<Point<long long>>a(n);
+  for(Point<long long>&x:a)std::cin>>x;
+  int m;
+  std::cin>>m;
+  std::vector<Point<long long>>b(m);
+  for(Point<long long>&x:b)std::cin>>x;
   CountPointsInTriangles<long long>c(a,b);
   int q;
   std::cin>>q;
