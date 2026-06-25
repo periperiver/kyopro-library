40e6af858641623a1b1f94ba74822be042e5635e
ShimoboTaiki <95344729+ShimoboTaiki@users.noreply.github.com>
2026-06-25 23:08:44 +0900

    a

--- graph/maximum_independent_set.hpp
+++ graph/maximum_independent_set.hpp
@@ -13,6 +13,7 @@ std::vector<int>maximum_independent_set(Graph<T>g){
   for(int i=0;i<n;i++)for(int j=i+1;j<n;j++)edge[i][j]=edge[j][i]=1;
   for(auto e:g)edge[e.from][e.to]=edge[e.to][e.from]=0;
   std::vector<int>deg(n);
+  for(int i=0;i<n;i++)for(int j=i+1;j<n;j++)if(edge[i][j])deg[i]++,deg[j]++;
   std::vector<std::vector<int>>col(n+1,std::vector<int>(n));
   std::vector<int>res;
   std::vector<Bs>buf(n+1);
@@ -30,8 +31,9 @@ std::vector<int>maximum_independent_set(Graph<T>g){
     }
     p++;
     for(int i=0;i<p;i++)buf[i].reset();
-    std::sort(rem.begin(),rem.end(),[&](int lhs,int rhs){return col[d][lhs]>col[d][rhs];});
-    for(int v:rem){
+    std::sort(rem.begin(),rem.end(),[&](int lhs,int rhs){return col[d][lhs]<col[d][rhs];});
+    while(!rem.empty()){
+      int v=rem.back();rem.pop_back();
       if(now.size()+col[d][v]+1<=res.size())break;
       std::vector<int>nrem;
       Bs bs;
