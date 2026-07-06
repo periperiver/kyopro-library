7a36af7dd80d649e3ddd4db9d474c5915745e10d
ShimoboTaiki <95344729+ShimoboTaiki@users.noreply.github.com>
2026-07-06 19:05:05 +0900

    discrete log

--- verify/self/math/discrete_logarithm.test.cpp
+++ verify/self/math/discrete_logarithm.test.cpp
@@ -1,6 +1,7 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
 #include "math/discrete_logarithm.hpp"
 #include "random/prime.hpp"
+#include "math/primitive_root.hpp"
 using mint=arbitrary_modint<long long,20260706>;
 template<bool f=false>
 void test(long long a,long long b,long long m){
@@ -18,16 +19,23 @@ void test(long long a,long long b,long long m){
   }
 }
 int main(){
-  for(int i=0;i<100;i++){
-    long long m=Random::range(1ll,1ll<<40);
+  for(int i=0;i<30;i++){
+    long long m=Random::range(1ll,1ll<<50);
     long long a=Random::range(m);
     long long b=Random::range(m);
     test(a,b,m);
   }
-  for(int i=0;i<100;i++){
-    long long m=Random::prime(1ll,1ll<<40);
+  for(int i=0;i<30;i++){
+    long long m=Random::prime(1ll,1ll<<50);
     long long a=primitive_root(m);
     long long b=Random::range(1ll,m);
+    test<true>(a,b,m);
+  }
+  for(int i=0;i<30;i++){
+    long long m=Random::prime(1ll,1ll<<50);
+    long long a=Random::range(m);
+    mint::set_mod(m);
+    long long b=mint(a).pow(Random::range(m)).val();
     test<true>(a,b,m);
   }
   int a,b;
