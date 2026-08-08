ec9b5933d0aaa635286e49de0cdd0c6e7b0c8485
ShimoboTaiki <95344729+ShimoboTaiki@users.noreply.github.com>
2026-08-08 17:55:02 +0900

    高速化

--- matrix/lll_algorithm.hpp
+++ matrix/lll_algorithm.hpp
@@ -37,9 +37,18 @@ void lll_algorithm(std::vector<std::vector<T>>&b){
   int m=b[0].size();
   assert(m>0);
   assert(std::all_of(b.begin(),b.end(),[&](const std::vector<T>&b){return (int)b.size()==m;}));
-  auto [b2,mu]=gram_schmidt(b);
+  std::vector<std::vector<Real>>b2(n,std::vector<Real>(m)),mu(n,std::vector<Real>(n));
   std::vector<Real>c(n);
-  for(int i=0;i<n;i++)c[i]=dot(b2[i],b2[i]);
+  auto eval=[&](int i)->void {
+    if(i==n)return;
+    std::copy(b[i].begin(),b[i].end(),b2[i].begin());
+    for(int j=0;j<i;j++){
+      mu[i][j]=dot(b[i],b2[j])/dot(b2[j],b2[j]);
+      add(b2[i],b2[j],-mu[i][j]);
+    }
+    c[i]=dot(b2[i],b2[i]);
+  };
+  eval(0),eval(1);
   for(int k=1;k<n;){
     bool f=false;
     for(int j=k-1;j>=0;j--){
@@ -50,13 +59,13 @@ void lll_algorithm(std::vector<std::vector<T>>&b){
         f=true;
       }
     }
-    if(f)std::tie(b2,mu)=gram_schmidt(b);
-    if(c[k]>(Real(3)/Real(4)-mu[k][k-1]*mu[k][k-1])*c[k-1])k++;
+    if(f)eval(k);
+    if(c[k]>(Real(3)/Real(4)-mu[k][k-1]*mu[k][k-1])*c[k-1])eval(++k);
     else{
       std::swap(b[k],b[k-1]);
-      std::tie(b2,mu)=gram_schmidt(b);
-      for(int i=0;i<n;i++)c[i]=dot(b2[i],b2[i]);
-      k=std::max(1,k-1);
+      eval(k-1);
+      if(k==1)eval(1);
+      else k--;
     }
   }
 }
