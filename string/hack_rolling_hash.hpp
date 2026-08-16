3b23ab322a4260bbd0d0363fb6c601892797ef82
ShimoboTaiki <95344729+ShimoboTaiki@users.noreply.github.com>
2026-08-16 16:11:22 +0900

    hack rolling hash

--- string/hack_rolling_hash.hpp
+++ string/hack_rolling_hash.hpp
@@ -1,21 +1,35 @@
 #pragma once
 #include<concepts>
 #include "../matrix/lll_algorithm.hpp"
-template<typename T>
-std::vector<T>hack_rolling_hash(T g,T mod,T sigma){
-  static_assert(std::is_signed_v<T>||std::is_same_v<T,__int128_t>);
-  for(int n=2;;n++){
-    std::vector<std::vector<T>>mat(n,std::vector<T>(n));
-    for(int i=0;i<n-1;i++){
-      mat[i][i]=-g;
-      mat[i][i+1]=1;
+#include "../math/primality_test.hpp"
+template<typename T,typename U>
+std::vector<std::make_signed_t<typename T::value_type>>hack_rolling_hash(std::vector<T>g,U sigma){
+  assert(!g.empty());
+  assert(sigma>0);
+  assert(isprime(T::mod()));
+  std::sort(g.begin(),g.end());
+  g.erase(std::unique(g.begin(),g.end()),g.end());
+  int n=g.size();
+  std::vector<T>dp(n+1);
+  dp[0]=1;
+  for(T v:g){
+    for(int i=n;i>=1;i--)dp[i]=dp[i-1]-dp[i]*v;
+    dp[0]*=-v;
+  }
+  using mat_type=std::conditional_t<(std::numeric_limits<typename T::value_type>::digits<=32),long long,__int128_t>;
+  for(int m=n+1;;m++){
+    std::vector<std::vector<mat_type>>mat(m,std::vector<mat_type>(m));
+    for(int i=0;i<m-n;i++){
+      for(int j=0;j<=n;j++)mat[i][i+j]=dp[j].val();
     }
-    mat[n-1][0]=mod;
+    for(int i=0;i<n;i++)mat[m-n+i][i]=T::mod();
     lll_algorithm(mat);
-    for(int i=0;i<n;i++){
-      const std::vector<T>&v=mat[i];
-      if(std::all_of(v.begin(),v.end(),[&sigma](const T&x){return (x>0?x:-x)<sigma;})){
-        return v;
+    for(int i=0;i<m;i++){
+      const std::vector<mat_type>&v=mat[i];
+      if(std::all_of(v.begin(),v.end(),[&sigma](const mat_type&x){return (x>0?x:-x)<sigma;})){
+        std::vector<std::make_signed_t<typename T::value_type>>res(m);
+        std::copy(v.begin(),v.end(),res.begin());
+        return res;
       }
     }
   }
--- verify/self/string/hack_rolling_hash.test.cpp
+++ verify/self/string/hack_rolling_hash.test.cpp
@@ -1,32 +1,57 @@
 #define PROBLEM "https://judge.yosupo.jp/problem/aplusb"
 #include<iostream>
 #include "string/hack_rolling_hash.hpp"
-#include "random/generator.hpp"
+#include "random/prime.hpp"
 #include "math/arbitrary_modint.hpp"
+#include "math/modint.hpp"
 constexpr int sigma=26;
-template<typename T>
-void test(T g,T mod){
-  using mint=arbitrary_modint<T,1>;
-  using type=std::conditional_t<(std::numeric_limits<T>::digits<=32),long long,__int128_t>;
-  auto a=hack_rolling_hash<type>(g,mod,sigma);
-  assert(std::any_of(a.begin(),a.end(),[](T x){return x!=0;}));
-  assert(std::all_of(a.begin(),a.end(),[&](T x){return (x>0?x:-x)<sigma;}));
-  mint::set_mod(mod);
-  mint sum=0;
-  for(int i=0;i<(int)a.size();i++)sum+=a[i]*mint::raw(g).pow(i);
-  assert(sum.val()==0);
+template<typename U,typename T>
+T eval(std::vector<U>f,T x){
+  T res=f.back();
+  for(int i=(int)f.size()-2;i>=0;i--)res=res*x+f[i];
+  return res;
 }
+template<typename T>
+void test(std::vector<T>g){
+  auto a=hack_rolling_hash(g,sigma);
+  assert(std::any_of(a.begin(),a.end(),[](auto x){return x!=0;}));
+  assert(std::all_of(a.begin(),a.end(),[&](auto x){return (x>0?x:-x)<sigma;}));
+  for(T v:g)assert(eval(a,v).val()==0);
+}
+using mint1=arbitrary_modint<int,1>;
+using mint2=arbitrary_modint<long long,1>;
 int main(){
-  for(int mod:{123457,998244353}){
-    for(int i=0;i<1000;i++){
-      int g=Random::range(1,mod);
-      test(g,mod);
+  for(int n=1;n<5;n++){
+    for(int i=0;i<30;i++){
+      std::vector<mint998>g;
+      for(int j=0;j<n;j++)g.push_back(Random::range(mint998::mod()));
+      test(g);
     }
-  }
-  for(long long mod:{92709568269121ll,(1ll<<61)-1}){
-    for(int i=0;i<1000;i++){
-      long long g=Random::range(1ll,mod);
-      test(g,mod);
+    for(int i=0;i<30;i++){
+      std::vector<mint107>g;
+      for(int j=0;j<n;j++)g.push_back(Random::range(mint107::mod()));
+      test(g);
+    }
+    for(int i=0;i<30;i++){
+      int mod=Random::prime(100,1000000000);
+      mint1::set_mod(mod);
+      std::vector<mint1>g;
+      for(int j=0;j<n;j++)g.push_back(Random::range(mod));
+      test(g);
+    }
+    if(n<=2){
+      for(int i=0;i<30;i++){
+        std::vector<mint61>g;
+        for(int j=0;j<n;j++)g.push_back(Random::range(mint61::mod()));
+        test(g);
+      }
+      for(int i=0;i<30;i++){
+        long long mod=Random::prime<long long>(1000000000,1000000000000000000);
+        mint2::set_mod(mod);
+        std::vector<mint2>g;
+        for(int j=0;j<n;j++)g.push_back(Random::range(mod));
+        test(g);
+      }
     }
   }
   int a,b;
