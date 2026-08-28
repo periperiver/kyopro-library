88a9197e7ce89088235ec546eb83ccfd670969e1
ShimoboTaiki <95344729+ShimoboTaiki@users.noreply.github.com>
2026-08-28 21:04:29 +0900

    kd tree

--- datastructure/kd_tree.hpp
+++ datastructure/kd_tree.hpp
@@ -1,122 +1,115 @@
 #pragma once
 #include<vector>
-#include<map>
 #include<algorithm>
 #include<numeric>
-#include "math/util.hpp"
+#include<cassert>
+#include "../math/util.hpp"
 template<typename I,typename M>
 struct kdTree{
-  static_assert(std::is_arithmetic_v<I>);
 private:
-  static constexpr I inf=std::numeric_limits<I>::max();
-  static constexpr I minf=std::numeric_limits<I>::min();
   using S=typename M::S;
   using F=typename M::F;
-  int z,log2n;
+  int n,z,log2n;
+  std::vector<std::pair<I,int>>xz,yz;
   std::vector<S>dat;
   std::vector<F>lazy;
-  std::vector<std::tuple<I,I,I,I>>rect;
   std::vector<int>pos;
-  void build(std::vector<std::tuple<I,I,S>>a){
-    int n=a.size();
-    z=ceil_pow2(n);
-    log2n=msb(z);
-    dat.resize(z*2,M::e());
-    lazy.resize(z*2,M::id());
-    rect.resize(z*2);
-    a.resize(z,std::make_tuple(I(),I(),M::e()));
-    std::vector<int>index(z);
-    std::iota(index.begin(),index.end(),0);
+  std::vector<std::tuple<int,int,int,int>>rect;
+  std::vector<int>len;
+  void propagate(int i,F f){
+    dat[i]=M::mapping(f,dat[i],len[i]);
+    if(i<z)lazy[i]=M::composition(f,lazy[i]);
+  }
+  inline void push(int id){
+    propagate(id*2,lazy[id]);
+    propagate(id*2+1,lazy[id]);
+    lazy[id]=M::id();
+  }
+  inline void update(int i){dat[i]=M::op(dat[i*2],dat[i*2+1]);}
+  S prod_rec(int id,int lx,int rx,int ly,int ry){
+    const auto&[lx2,rx2,ly2,ry2]=rect[id];
+    if(lx<=lx2&&rx2<=rx&&ly<=ly2&&ry2<=ry)return dat[id];
+    if(rx2<=lx||rx<=ly2||ry2<=ly||ry<=ly2)return M::e();
+    push(id);
+    return M::op(prod_rec(id*2,lx,rx,ly,ry),prod_rec(id*2+1,lx,rx,ly,ry));
+  }
+  void apply_rec(int id,int lx,int rx,int ly,int ry,F f){
+    const auto&[lx2,rx2,ly2,ry2]=rect[id];
+    if(lx<=lx2&&rx2<=rx&&ly<=ly2&&ry2<=ry)return propagate(id,f);
+    if(rx2<=lx||rx<=ly2||ry2<=ly||ry<=ly2)return;
+    push(id);
+    apply_rec(id*2,lx,rx,ly,ry,f);
+    apply_rec(id*2+1,lx,rx,ly,ry,f);
+    update(id);
+  }
+  inline int getx(I x)const{return std::lower_bound(xz.begin(),xz.end(),std::make_pair(x,0))-xz.begin();}
+  inline int gety(I y)const{return std::lower_bound(yz.begin(),yz.end(),std::make_pair(y,0))-yz.begin();}
+public:
+  kdTree(){}
+  kdTree(std::vector<std::tuple<I,I,S>>init):n(init.size()),z(ceil_pow2(n)),log2n(msb(z)),xz(n),yz(n),dat(z*2,M::e()),lazy(z,M::id()),pos(n),rect(z*2),len(z*2){
+    for(int i=0;i<n;i++){
+      const auto&[x,y,v]=init[i];
+      xz[i]=std::make_pair(x,i);
+      yz[i]=std::make_pair(y,i);
+    }
+    std::sort(xz.begin(),xz.end()),std::sort(yz.begin(),yz.end());
+    std::vector<std::tuple<int,int,int>>idx(n);
+    for(int i=0;i<n;i++){
+      auto&[x,y,v]=init[i];
+      x=std::lower_bound(xz.begin(),xz.end(),std::make_pair(x,i))-xz.begin();
+      y=std::lower_bound(yz.begin(),yz.end(),std::make_pair(y,i))-yz.begin();
+      idx[i]=std::make_tuple(i,x,y);
+    }
+    std::vector<std::pair<int,int>>lr(z*2);
+    lr[1]=std::make_pair(0,n);
     for(int i=1;i<z;i++){
-      int m=msb(i);
-      int l=(i^(1<<m))<<(log2n-m);
-      int r=l+(1<<(log2n-msb(i)));
-      if(i==1){
-        I mnx=inf,mxx=minf,mny=inf,mxy=minf;
-        for(const auto&[x,y,v]:a){
-          if(mnx>x)mnx=x;
-          if(mxx<x)mxx=x;
-          if(mny>y)mny=y;
-          if(mxy<y)mxy=y;
-        }
-        rect[1]=std::make_tuple(mnx,mxx,mny,mxy);
-      }
-      rect[i*2]=rect[i*2+1]=rect[i];
-      if(m&1){
-        std::nth_element(index.begin()+l,index.begin()+(l+r)/2,index.begin()+r,[&](int x,int y){return std::get<1>(a[x])<std::get<1>(a[y]);});
-        std::get<3>(rect[i*2])=std::get<2>(rect[i*2+1])=std::get<1>(a[index[(l+r)/2]]);
+      auto [l,r]=lr[i];
+      if(msb(i)%2==0){
+        std::sort(idx.begin()+l,idx.begin()+r,[&](const std::tuple<int,int,int>&lhs,const std::tuple<int,int,int>&rhs){return std::get<1>(lhs)<std::get<1>(rhs);});
+        int m=(l+r)/2;
+        lr[i*2]=std::make_pair(l,m);
+        lr[i*2+1]=std::make_pair(m,r);
       }
       else{
-        std::nth_element(index.begin()+l,index.begin()+(l+r)/2,index.begin()+r,[&](int x,int y){return std::get<0>(a[x])<std::get<0>(a[y]);});
-        std::get<1>(rect[i*2])=std::get<0>(rect[i*2+1])=std::get<0>(a[index[(l+r)/2]]);
+        std::sort(idx.begin()+l,idx.begin()+r,[&](const std::tuple<int,int,int>&lhs,const std::tuple<int,int,int>&rhs){return std::get<2>(lhs)<std::get<2>(rhs);});
+        int m=(l+r)/2;
+        lr[i*2]=std::make_pair(l,m);
+        lr[i*2+1]=std::make_pair(m,r);
+      }
+      len[i]=r-l;
+    }
+    for(int i=z;i<z*2;i++){
+      if(lr[i].first!=lr[i].second){
+        auto [id,x,y]=idx[lr[i].first];
+        dat[i]=std::get<2>(init[id]);
+        pos[id]=i;
+        rect[i]=std::make_tuple(x,x+1,y,y+1);
+        len[i]=1;
       }
     }
-    pos.resize(z);
-    for(int i=0;i<z;i++){
-      const auto&[x,y,v]=a[index[i]];
-      dat[i+z]=v;
-      pos[index[i]]=i;
-      rect[i+z]=std::make_tuple(x,x,y,y);
+    for(int i=z;--i;){
+      update(i);
+      const auto&[lx1,rx1,ly1,ry1]=rect[i*2];
+      const auto&[lx2,rx2,ly2,ry2]=rect[i*2+1];
+      rect[i]=std::make_tuple(std::min(lx1,lx2),std::max(rx1,rx2),std::min(ly1,ly2),std::max(ry1,ry2));
     }
-    for(int i=z-1;i>=1;i--)update(i);
   }
-  inline void update(int i){
-    dat[i]=M::op(dat[i*2],dat[i*2+1]);
-  }
-  inline void propagate(int i,const F&f){
-    dat[i]=M::mapping(f,dat[i],log2n-msb(i));
-    lazy[i]=M::composition(f,lazy[i]);
-  }
-  inline void push(int i){
-    propagate(i*2,lazy[i]);
-    propagate(i*2+1,lazy[i]);
-    lazy[i]=M::id();
-  }
-public:
-  kdTree(const std::vector<std::tuple<I,I,S>>&init){
-    build(init);
+  void set(int i,S v){
+    dat[i=pos[i]]=v;
+    for(int j=log2n;j>=1;j--)push(i>>j);
+    while(i>>=1)update(i);
   }
   S prod(I lx,I rx,I ly,I ry){
-    rx--,ry--;
-    S res=M::e();
-    auto dfs=[&](auto self,int id)->void {
-      const auto&[nlx,nrx,nly,nry]=rect[id];
-      if(rx<nlx||nrx<lx||ry<nly||nry<ly)return;
-      if(lx<=nlx&&nrx<=rx&&ly<=nly&&nry<=ry){
-        res=M::op(res,dat[id]);
-        return;
-      }
-      if(id<z){
-        push(id);
-        self(self,id*2);
-        self(self,id*2+1);
-      }
-    };
-    dfs(dfs,1);
-    return res;
+    assert(lx<=rx);
+    assert(ly<=ry);
+    lx=getx(lx),rx=getx(rx),ly=gety(ly),ry=gety(ry);
+    if(lx==rx||ly==ry)return M::e();
+    else return prod_rec(1,lx,rx,ly,ry);
   }
-  void apply(I lx,I rx,I ly,I ry,const F&f){
-    rx--,ry--;
-    auto dfs=[&](auto self,int id)->void {
-      const auto&[nlx,nrx,nly,nry]=rect[id];
-      if(rx<nlx||nrx<lx||ry<nly||nry<ly)return;
-      if(lx<=nlx&&nrx<=rx&&ly<=nly&&nry<=ry){
-        propagate(id,f);
-        return;
-      }
-      if(id<z){
-        push(id);
-        self(self,id*2);
-        self(self,id*2+1);
-        update(id);
-      }
-    };
-    dfs(dfs,1);
-  }
-  void set(int id,const S&x){
-    id=pos[id]+z;
-    for(int i=log2n;i>=1;i--)push(id>>i);
-    dat[id]=x;
-    for(int i=1;i<=log2n;i++)update(id>>i);
+  void apply(I lx,I rx,I ly,I ry,F f){
+    assert(lx<=rx);
+    assert(ly<=ry);
+    lx=getx(lx),rx=getx(rx),ly=gety(ly),ry=gety(ry);
+    if(lx<rx&&ly<ry)apply_rec(1,lx,rx,ly,ry,f);
   }
 };
